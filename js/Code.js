9ae5b4f4d79a9d253a6a2b1f91dcc9d51ff46c77
Satyamlap <91893612+Satyamlap@users.noreply.github.com>
2022-07-02 20:53:02 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "GOOGLE";
+const accessCode2 = "CODING";
+const accessCode3 = "EARTH";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Which Website Know EveryThing, Letter From G !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: What is Your Passion, Letter From C  !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: 3rd Planet of Your Solar System !!", 100,270)
+
+}
